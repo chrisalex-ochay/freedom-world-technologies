6c9eb890c1f904a57954f65d4a1999e43c56ca59
chrisalex-ochay <69009810+chrisalex-ochay@users.noreply.github.com>
2021-07-28 07:10:08 +0300

    Create header.php

---
+++ php/header.php
@@ -0,0 +1,100 @@
+<!-- -------------------------header1 starts-------------------- --- -->
+<header class="header1">
+        <div class="container1">
+            <div class="header_left">
+                <a href="../index.php" class="mylogo"><img src="../images/my-logo-3.png" alt=""></a>
+            </div>
+            <div class="header_right">
+                <label for="navTrigger" class="nav_icon"><i class="fa fa-bars"></i></label>
+                <input id="navTrigger" type="checkbox" name="">
+              <nav class="main_nav">
+                  <ul>
+                      <li><a href="../index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+                      <li><a href="../products-page1.php"><span class="glyphicon glyphicon-asterisk "></span>Products</a></li>
+                      <li><a href="#"><span class="glyphicon glyphicon-globe"></span>Services</a>
+                        <i class="fa fa-sort-desc"></i>
+                        <ul>
+                            <li><a href="#">Website Development</a>
+                                <i class="fa fa-sort-desc"></i>
+                                <ul>
+                                    <li><a href="#">Personal Website</a></li>
+                                    <li><a href="#">Company Website</a></li>
+                                    <li><a href="#">E-commerce Website</a></li>
+                                    <li><a href="#">NGO Website</a></li>
+                                    <li><a href="#">And More</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Mobile App Development</a></li>
+                            <li><a href="#">Software/Systems Dev't</a>
+                                <i class="fa fa-sort-desc"></i>
+                                <ul>
+                                    <li><a href="#">ERP program</a></li>
+                                    <li><a href="#">Sacco Mgt System</a></li>
+                                    <li><a href="#">POS system</a></li>
+                                    <li><a href="#">Any More</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Software Installations</a>
+                                <i class="fa fa-sort-desc"></i>
+                                <ul>
+                                    <li><a href="#">Asycuda World</a></li>
+                                    <li><a href="#">MS Windows OS</a>
+                                        <i class="fa fa-sort-desc"></i>
+                                        <ul>
+                                            <li><a href="#">Windows 10</a></li>
+                                            <li><a href="#">Windows 8.1</a></li>
+                                            <li><a href="#">Windows 8</a></li>
+                                            <li><a href="#">Windows 7</a></li>
+                                            <li><a href="#">Windows XP</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Linux</a></li>
+                                    <li><a href="#">Microsoft office</a>
+                                        <i class="fa fa-sort-desc"></i>
+                                        <ul>
+                                            <li><a href="#">Office 2019</a></li>
+                                            <li><a href="#">Office 2016</a></li>
+                                            <li><a href="#">Office 2013</a></li>
+                                            <li><a href="#">Office 2010</a></li>
+                                            <li><a href="#">Office 2007</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Anti-virus</a></li>
+                                    <li><a href="#">And Many More</a></li>
+                                </ul>    
+                            </li>
+                            <li><a href="#">ISP services</a>
+                                <i class="fa fa-sort-desc"></i>
+                                <ul>
+                                    <li><a href="#">Zuku Internet</a></li>
+                                    <li><a href="#">Smart Mobile Internet</a></li>
+                                    <li><a href="#">Simba Telecom</a></li>
+                                    <li><a href="#">And Others</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Automatic Timetable Dev't</a></li>
+                            <li><a href="#">Computer Literacy lessons</a>
+                                <i class="fa fa-sort-desc"></i>
+                                <ul>
+                                    <li><a href="#">MS Word,Excel etc</a></li>
+                                    <li><a href="#">Programming</a></li>
+                                    <li><a href="#">Database</a></li>
+                                    <li><a href="#">And Others</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Internet Cafe Services</a></li>
+                            <li><a href="#">Phone/Computer Repairing</a></li>
+                            <li><a href="#">General I.T consultacy</a></li>
+                        </ul>  
+                      </li>
+                      <li><a href="../aboutus.php"><span class="glyphicon glyphicon-info-sign"></span>About Us</a></li>
+                      <li><a href="../contactus.php"><span class="glyphicon glyphicon-phone-alt"></span>Contact Us</a></li>
+                      <li><a href="../account.php"><span class="glyphicon glyphicon-user"></span>Account</a></li>
+                      <li><a href="../cart.php" class="fas fa-shopping-cart"></a></li>
+                  </ul>
+              </nav>
+            </div>
+        </div>
+    </header>
+
+    <!-- -------------------------header1 ends-------------------- --- -->
