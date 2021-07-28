ff814a1b96de0dc9263867805145b58e61fa0a1f
chrisalex-ochay <69009810+chrisalex-ochay@users.noreply.github.com>
2021-07-28 07:04:49 +0300

    Create aboutus.php

---
+++ php/aboutus.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>About Us - Freedom World Technolories & Accesories</title>
+            <link rel="stylesheet" href="style.css">
+        
+            <!-- Link for awesome fonts cdn -->
+            <link href="https://fonts.googleapis.com/css2?
+            family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"> 
+        
+            <!-- link for icon font awesome -->
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" 
+            integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        
+            <!-- Link for starts ratings from fonts cdn -->
+            <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/
+            fontawesome.min.css"> -->
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+            
+            <!-- owl carousel css file cdn link -->
+            <link rel="stylesheet" href="https:cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+        
+            <!-- Bootstrap and J-query library links -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        
+        </head>
+    <body>
+               
+<!-- ------------------------- php header link --------------------------  -->
+    <?php include('header2.php'); ?>
+
+    
+<!-- --------------------- about us start --------------------------- -->
+
+    <div class="aboutus-section">
+        <div class="container">
+            <div class="content-section col-2">
+                <div class="title">
+                    <h1>About Us</h1>
+                </div>
+                <div class="content">
+                    <h3>We are an I.T company who deal in quality and standard all-round I.T technological gadgets/materials
+                        and services. </h3><br>
+                    <h4 style="font-weight: bolder;">Our Vision</h4> 
+                       <p> Our vision is to serve, empower, enrich and produce quality technologies, standard electronics and the finest
+                            accesories for local and global consumption. </p><br>
+                    <h4 style="font-weight: bolder;">Our Mision</h4>
+                        <p>   Our mision is to provide services and empower every one in there capacities from basic to advanced 
+                            Technology, enrich ugandans, africans and the world at large with standard and quatity technologies 
+                            and utilise all available resources in our societies to automate and technologically make 
+                            lives easy to live in this world.</p>
+                </div>
+                <div class="social">
+                    <a href=""><i class="fab fa-facebook"></i></a>
+                    <a href=""><i class="fab fa-twitter"></i></a>
+                    <a href=""><i class="fab fa-instagram"></i></a>
+                    <a href=""><i class="fab fa-youtube"></i></a>
+                    <a href=""><i class="fab fa-tiktok"></i></a>
+                </div>
+            </div>
+            <div class="image-section col-2">
+                <img src="images/profile-pic-dummy.png" alt="">
+            </div>
+        </div>
+    </div>
+    <!-- --------------------- about us ends --------------------------- -->
+
+<!-- --------------------- footer php link --------------------------- -->
+    <?php include('footer2.php'); ?>
+
+<!---------------    jquery cdn link    --------------->
+<script src="https:cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!---------------    owl carousel js file cdn link    --------------->
+<script src="https:cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+<script src="main.js"></script>
+
+
+</body>
+</html>
