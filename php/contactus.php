95ee34a8c32f674a432e15b37808769567ecbd84
chrisalex-ochay <69009810+chrisalex-ochay@users.noreply.github.com>
2021-07-28 07:07:22 +0300

    Create contactus.php

---
+++ php/contactus.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Contact Us - Freedom World Technolories & Accesories</title>
+            <link rel="stylesheet" href="style.css">
+        
+            <!-- Link for awesome fonts cdn -->
+            <link href="https://fonts.googleapis.com/css2?
+            family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"> 
+        
+            <!-- link for icon font awesome -->
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" 
+            integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        
+            <!-- Link for starts ratings from fonts cdn -->
+            <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/
+            fontawesome.min.css"> -->
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+            
+            <!-- owl carousel css file cdn link -->
+            <link rel="stylesheet" href="https:cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+        
+             <!-- Bootstrap and J-query library links -->
+             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        </head>
+    <body>
+          
+       
+<!-- ------------------------- php header link --------------------------  -->
+    <?php include('header2.php'); ?>
+    
+    
+    <!-- --------------------- contact us start --------------------------- -->
+
+    <div class="contactus-section">
+        <div class="container">
+            <div class="image-section-contact col-2">
+                <img src="images/profile-pic-dummy.png" alt="">
+            </div>
+            <div class="content-section-contactus col-2">
+                <div class="title">
+                    <h1>Contact Us</h1>
+                </div>
+                <div class="content-contactus">
+                    
+                    <h4 style="font-weight: bolder;">freedomworldtechnologies.com </h4> 
+                    <h4 style="font-weight: bolder;">+(256)-703-200-934</h4>
+                    <h4 style="font-weight: bolder;">+(256)-787-200-935</h4>
+                    <h4 style="font-weight: bolder;">chrisalexfree51@gmail.com</h4> <br>
+                    <h4 style="font-weight: bolder;">chris.a@freedomworldtechnologies.com</h4>   
+                    <h4 style="font-weight: bolder;">baker@freedomworldtechnologies.com</h4> 
+                    <h4 style="font-weight: bolder;">henry@freedomworldtechnologies.com</h4>   
+                    <h4 style="font-weight: bolder;">jackie@freedomworldtechnologies.com</h4> <br>
+                                       
+                </div>
+                <div class="social">
+                    <a href=""><i class="fab fa-facebook"></i></a>
+                    <a href=""><i class="fab fa-twitter"></i></a>
+                    <a href=""><i class="fab fa-instagram"></i></a>
+                    <a href=""><i class="fab fa-youtube"></i></a>
+                    <a href=""><i class="fab fa-tiktok"></i></a>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+    <!-- --------------------- Contact us ends --------------------------- -->
+
+
+<!-- --------------------- footer php link --------------------------- -->
+<?php include('footer2.php'); ?>
+
+
+<!---------------    jquery cdn link    --------------->
+<script src="https:cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!---------------    owl carousel js file cdn link    --------------->
+<script src="https:cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+<script src="main.js"></script>
+
+</body>
+</html>
