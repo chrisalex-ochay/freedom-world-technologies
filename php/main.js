fd41a5a5b5c69a1243f80c9e5e39348a9be0a81d
chrisalex-ochay <69009810+chrisalex-ochay@users.noreply.github.com>
2021-07-28 07:16:19 +0300

    Create main.js

---
+++ php/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $('.home-slider').owlCarousel({
+        items:1,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout:5000,
+        loop:true
+    });
+
+});
