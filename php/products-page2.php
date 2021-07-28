220958fa6dfa94d721dfe5cfc3a99028b476af30
chrisalex-ochay <69009810+chrisalex-ochay@users.noreply.github.com>
2021-07-28 07:18:47 +0300

    Create products-page2.php

---
+++ php/products-page2.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>All Products-Page2 - Freedom World Technolories & Accesories</title>
+        <link rel="stylesheet" href="style.css">
+    
+        <!-- Link for awesome fonts cdn -->
+        <link href="https://fonts.googleapis.com/css2?
+        family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"> 
+    
+        <!-- link for icon font awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" 
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    
+        <!-- Link for starts ratings from fonts cdn -->
+        <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/
+        fontawesome.min.css"> -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+        <!-- owl carousel css file cdn link -->
+        <link rel="stylesheet" href="https:cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    
+        <!-- Bootstrap and J-query library links -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+<body>
+      
+      <!-- ------------------------- php header link --------------------------  -->
+      <?php include('header2.php'); ?>
+    
+
+<!-----------------------------        Featured Products       ---------------------->
+    <div class="small-container">
+        <div class="row row-2">
+            <h2>All Products Page2</h2>
+            <select>
+                <option>Default Shorting</option>
+                <option>Short by price</option>
+                <option>Short by popularity</option>
+                <option>Short by rating</option>
+                <option>Short by sale</option>
+            </select>
+        </div>
+        
+        <div class="row">
+            <div class="col-4">
+                <a href="productfiles/product-17-percolator2golden-details.php"><img src="images/product-17-percolator-golden1.png"></a>
+                <a href="productfiles/product-17-percolator2golden-details.php"><h4>Golden Percolators</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>$12.00-$35.00</p>
+            </div>
+            <div class="col-4">
+                <a href="productfiles/product-18-chargerovs-details.php"><img src="images/product-18-multi-charer-ovs1.png"></a>
+                <a href="productfiles/product-18-chargerovs-details.php"><h4>Multi-charger OVS</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half-o"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>$5.00-$10.00</p>
+            </div>
+            <div class="col-4">
+                <a href="productfiles/product-19-headphonesjbl1-details.php"><img src="images/product-19-headphones-jbl1.png"></a>
+                <a href="productfiles/product-19-headphonesjbl1-details.php"><h4>Wireless headphones-jbl</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half-o"></i>
+                </div>
+                <p>$15.00-$20.00</p>
+            </div>
+            <div class="col-4">
+                <a href="productfiles/product-20-screenguards-details.php"><img src="images/product-20-glass-screenguards1.png"></a>
+                <a href="productfiles/product-20-screenguards-details.php"><h4>Glass Screen guards</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half-o"></i>
+                </div>
+                <p>$5.00-$8.00</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <a href="productfiles/product-21-cupflask2-details.php"><img src="images/product-21-flask-stainless-1cup1.png"></a>
+                <a href="productfiles/product-21-cupflask2-details.php"><h4>1 cup stainless flask</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>$6.00-$15.00</p>
+            </div>
+            <div class="col-4">
+                <a href="productfiles/product-22-mug1-details.php"><img src="images/product-22-mug-traveling-cup1.png"></a>
+                <a href="productfiles/product-22-mug1-details.php"><h4>traveling mug 1 cup</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half-o"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>$8.00-$12.00</p>
+            </div>
+            <div class="col-4">
+                <a href="productfiles/product-23-blendersaachi-details.php"><img src="images/product-23-blender-saachi-1.png"></a>
+                <a href="productfiles/product-23-blendersaachi-details.php"><h4>2 jag saachi blender</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half-o"></i>
+                </div>
+                <p>$50.50-$60.00</p>
+            </div>
+            <div class="col-4">
+                <a href="productfiles/product-24-laptopbug-details.php"><img src="images/product-24-laptop-bug-1.png"></a>
+                <a href="productfiles/product-24-laptopbug-details.php"><h4>Laptop Bugs</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>$14.00-$24.00</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-4">
+                <a href="productfiles/product-25-bluetooth1-details.php"><img src="images/product-25-bluetooth-earphones-jbl1.png"></a>
+                <a href="productfiles/product-25-bluetooth1-details.php"><h4>Wireless headphones-jbl</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half-o"></i>
+                </div>
+                <p>$15.00-$20.00</p>
+            </div>
+            <div class="col-4">
+                <a href="productfiles/product-26-plugs1-details.php"><img src="images/product-26-plugs1.png"></a>
+                <a href="productfiles/product-26-plugs1-details.php"><h4>Plugs</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half-o"></i> 
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>$5.00-$10.00</p>
+            </div>
+            <div class="col-4">
+                <a href="productfiles/product-27-adapters1-details.php"><img src="images/product-27-adapters1.png"></a>
+                <a href="productfiles/product-27-adapters1-details.php"><h4>Adapter/Plugs</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half-o"></i>
+                </div>
+                <p>$5.00-$10.00</p>
+            </div>
+            <div class="col-4">
+                <a href="productfiles/product-28-phonecovers1-details.php"><img src="images/product-28-phone-backcovers1.png"></a>
+                <a href="productfiles/product-28-phonecovers1-details.php"><h4>Phone Covers</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>$5.00-$10.00</p>
+            </div>
+        </div> 
+
+        <div class="row">
+            <div class="col-4">
+                <a href="productfiles/product-29-extensions1-details.php"><img src="images/product-29-extensions-1.png"></a>
+                <a href="productfiles/product-29-extensions1-details.php"><h4>Villaon Extensions</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i> 
+                </div>
+                <p>$10.00-$15.00</p>
+            </div>
+            <div class="col-4">
+                <a href="productfiles/product-31-simtablets-details.php"><img src="images/product-31-simcard-enabled-tablets1.png"></a>
+                <a href="productfiles/product-31-simtablets-details.php"><h4>4G simcard TAB</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half-o"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>$85.00-$100.00</p>
+            </div>
+            <div class="col-4">
+                <a href="productfiles/product-32-wirelessadapters-details.php"><img src="images/product-32-wireless-adapters1.png"></a>
+                <a href="productfiles/product-32-wirelessadapters-details.php"><h4>Wireless USB adapters</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half-o"></i>
+                </div>
+                <p>$22.00-$25.00</p>
+            </div>
+            <div class="col-4">
+                <a href="productfiles/product-30-headphonesjbl2-details.php"><img src="images/product-30-headphones-jbl1.png"></a>
+                <a href="productfiles/product-30-headphonesjbl2-details.php"><h4>Headphones</h4></a>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>$15.00-$20.00</p>
+            </div>
+        </div> 
+
+        <div class="page-btn">
+            <a href="products-page1.php"><span>1</span></a>
+            <a href="products-page2.php"><span>2</span></a>
+            <a href="products-page3.php"><span>3</span></a>
+            <span>&#8594</span>
+        </div>
+    </div>
+
+<!----------------------    footer php link  ---------------------->
+<?php include('footer2.php'); ?>
+
+<!---------------    jquery cdn link    --------------->
+<script src="https:cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!---------------    owl carousel js file cdn link    --------------->
+<script src="https:cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+<script src="main.js"></script>
+
+</body>
+</html>
