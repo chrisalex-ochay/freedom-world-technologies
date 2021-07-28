2754296c15d3f0d2d2494893d035df02cc20caef
chrisalex-ochay <69009810+chrisalex-ochay@users.noreply.github.com>
2021-07-28 07:08:41 +0300

    Create footer.php

---
+++ php/footer.php
@@ -0,0 +1,51 @@
+
+<div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-1">
+                    <h3>Download our App</h3>
+                    <p>Download App for Android and ios mobile phone<br> soon coming.</p>
+                    <div class="app-logo">
+                        <img src="../images/play-store.png">
+                        <img src="../images/app-store.png">
+                    </div>
+                </div>
+                <div class="footer-col-2">
+                    <a href="../index.php"><img src="../images/my-logo-1.png"></a>
+                    <p>Our vision is to serve, empower, enrich and produce quality technologies, standard electronics and the finest
+                        accesories for local and global consumption.
+                    </p>
+                </div>
+                <div class="footer-col-3">
+                    <h3>Quality Products</h3>
+                    <ul>
+                        <li><a href="../products-page1.php">Page1</a></li>
+                        <li><a href="../products-page2.php">Page2</a></li>
+                        <li><a href="../products-page3.php">Page3</a></li>
+                        <li><a href="../index.php">Featured prdts</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col-4">
+                    <h3>Alternatitive links</h3>
+                    <ul>
+                        <li><a href="../index.php">Home</a></li>
+                        <li><a href="../aboutus.php">Our Mision/Vision</a></li>
+                        <li><a href="../contactus.php">Connect with us</a></li>
+                        <li><a href="../account.php">Create an acount with us</a></li>
+                        
+                    </ul>
+                </div>
+                <div class="footer-col-5">
+                    <h3>Follow us</h3>
+                    <ul>
+                        <li>Facebook</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                        <li>Youtube</li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <p class="copyright"> Copyright 2021 - Freedom World Technologies</p>
+        </div>
+    </div>
